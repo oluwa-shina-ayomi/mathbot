808b6cfac41c5b9cd6aeadfdd9bf596fa0f174b8
Abdulrahmon Adebayo <abdulrahmon11052008@gmail.com>
2025-06-26 14:19:12 +0000

    Initial version of MathBot with greet function

---
+++ mathbot.py
@@ -0,0 +1,20 @@
+def greet ():
+    print ("Hi 👋 ")
+    x = input ("what's your name? ".strip().title)
+    print (f"Hello! {x}. I am Mathbot. I can help you with basic math.")
+    print ("What do you want to do today")
+    print ("1. Add numbers")
+    print ("2. Subtract numbers")
+    print ("3. multiply numbers")
+    print ("4. divide numbers")
+    print ("5. Learn a Math concept")
+    print ("6. Exit")
+
+
+
+
+
+
+
+
+greet ()
